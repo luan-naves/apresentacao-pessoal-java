cdc4944b71b083caca875337e721248c2c3689ab
Luan Naves <luannf-@hotmail.com>
2025-08-10 03:27:33 -0300

    troca sout

--- src/main/java/com/mycompany/apresentacaopessoaljava/ApresentacaoPessoalJava.java
+++ src/main/java/com/mycompany/apresentacaopessoaljava/ApresentacaoPessoalJava.java
@@ -11,6 +11,6 @@ package com.mycompany.apresentacaopessoaljava;
 public class ApresentacaoPessoalJava {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Olá, mundo!");
     }
 }
